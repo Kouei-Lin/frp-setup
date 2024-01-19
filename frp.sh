900bd8f04f6ad88696306557dacd61312900409d
Kouei <hungjui11@gmail.com>
2024-01-19 17:08:50 +0800

    frp.sh edit

--- frp.sh
+++ frp.sh
@@ -15,11 +15,11 @@ mv frp_0.44.0_linux_arm64 frp
 # 移動frpc.ini至frp
 mv ~/frp-setup/frpc.ini /srv/frp/
 
+# 啟動frp利用frpc.ini
+/srv/frp/frpc/ -c /srv/frp/frpc.ini
+
 # 將frpc_22.conf移動至/etc/supervisor/conf.d
 mv ~/frp-setup/frpc_22.conf /etc/supervisor/conf.d/
 
 # 重啟supervisor
 supervisorctl reload
-
-# 重開機
-reboot
