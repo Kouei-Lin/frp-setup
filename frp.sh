aebb09de80ab31532442454965a802e3ff90d8b0
Kouei <hungjui11@gmail.com>
2024-01-19 15:33:28 +0800

    frp.sh typo fix

--- frp.sh
+++ frp.sh
@@ -16,7 +16,7 @@ mv frp_0.44.0_linux_arm64 frp
 mv ~/frp-setup/frpc.ini /srv/frp/
 
 # 啟動frpc利用定義之frpc.ini
-/srv/frp/frpc -c /srv/frp/frpc.ini
+/srv/frp/frpc -c frpc.ini
 
 # 將frpc_22.conf移動至/etc/supervisor/conf.d
 mv ~/frp-setup/frpc_22.conf /etc/supervisor/conf.d/
