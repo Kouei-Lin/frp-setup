7df187d17e7fef707b97a7dde9acd5214793f853
Kouei <hungjui11@gmail.com>
2024-01-22 16:46:12 +0800

    mod frp.sh

--- frp.sh
+++ frp.sh
@@ -7,7 +7,8 @@ FRP_ARCH="arm64"
 # Move to /srv
 cd /srv && \
 # Download frp with dynamic version and architecture
-wget "https://github.com/fatedier/frp/releases/download/v$FRP_VERSION/frp_${FRP_VERSION}_linux_$FRP_ARCH.tar.gz" && \
+wget "https://github.com/fatedier/frp/releases/download/v$FRP_VERSION/frp_${FRP_VERSION}_linux_$FRP_ARCH.tar.gz
+" && \
 # Extract the tar file
 tar xvzf "frp_${FRP_VERSION}_linux_$FRP_ARCH.tar.gz" && \
 # Rename the extracted directory to 'frp'
@@ -21,7 +22,7 @@ server_port = SERVER_PORT_!!
 admin_addr = ADMIN_ADDR_!!
 admin_port = ADMIN_PORT_!!
 
-[ssh_SERVICE_NAME!!]
+[ssh_SERVICE_NAME_!!]
 type = tcp
 local_ip = 127.0.0.1
 local_port = 22
@@ -49,4 +50,3 @@ WantedBy=multi-user.target
 sudo systemctl daemon-reload && \
 sudo systemctl enable frpc.service && \
 sudo systemctl start frpc.service
-
